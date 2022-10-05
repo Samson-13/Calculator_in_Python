36b4fb5d95691dc3e8d2198177c52d455347d367
M Samson Badding <85184417+Samson-13@users.noreply.github.com>
2022-10-05 11:20:25 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,30 +1,51 @@
-print("***Calculator***")
+# Addition
 def add(a,b):
-    return a+b
+    return '\nAdditon of {} + {} : {}'.format(a, b, a + b)
+
+# Subtraction
 def subtract(a, b):
-    return a-b
+    return '\nSubtraction of {} - {} : {}'.format(a, b, a - b)
+
+# Multiplication
 def multiply(a, b):
-    return  a*b
+    return  '\nMultiplication of {} X {} : {}'.format(a, b, a * b)
+
+# Division
 def divide(a, b):
-    return a/b
+    c = float(a)
+    d = float(b)
+    return '\nDivision of {} by {} : {}'.format(a, b, c / d)
 
+# Modulus
+def mod(a, b):
+    return '\nModulus : {}'.format(a % b)
 
-x=int(input("Enter the first number: "))
-y=int(input("Enter the second number: "))
-print("Choose the operator")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
-ch=int(input("Enter your operator: "))
-if ch== 1:
-    print(add(x, y))
-elif ch== 2:
-    print(subtract(x,y))
-elif ch== 3:
-    print(multiply(x,y))
-elif ch== 4:
-    print(divide(x,y))
-else:
-    print("Unknown operator...")
-
+# MAIN
+num1=int(input("\nEnter the first number: "))
+num2=int(input("Enter the second number: "))
+while True:
+    print("\nChoose the operator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Modulus")
+    print("6. Exit")
+    ch = int(input("\nEnter your operator: "))
+    if ch == 1:
+        print(add(num1, num2))
+    elif ch == 2:
+        print(subtract(num1,num2))
+    elif ch == 3:
+        print(multiply(num1,num2))
+    elif ch == 4:
+        print(divide(num1,num2))
+    elif ch == 5:
+        print(mod(num1,num2))
+    elif ch == 6:
+        print("Exiting.....")
+        break
+    elif ch > 6 or ch < 0:
+        print("Invalid operator")
+    else:
+        print("Invalid operator")
